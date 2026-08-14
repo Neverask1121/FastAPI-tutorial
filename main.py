6c97d63c28668110070b0d2eb8f12d6039ddf307
Neverask1121 <rpsh2823@ramadevipublicschool.com>
2026-08-15 02:48:27 +0530

    not a plan to use gpt for this

--- main.py
+++ main.py
@@ -4,3 +4,6 @@ app = FastAPI()
 def home():
   return {"message", "Hello bro how are you I am fine thank you"}
 
+@app.get("/about")
+def notabout():
+  return {"explaination" : "cricket score predictor model", "Version" : "1.0"}
