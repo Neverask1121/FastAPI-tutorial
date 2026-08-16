46648447f308cbce4994422537ac9857a5fcb5c4
Neverask1121 <rpsh2823@ramadevipublicschool.com>
2026-08-16 15:32:22 +0530

    hello world

--- query_p.py
+++ query_p.py
@@ -4,15 +4,23 @@ app = FastAPI()
 
 customer_risk_level = [
   {"name" : "Aditya", "city" :"bangalore", "risk" : "high"},
-  {"name" : "Aditya", "city" :"mangalore", "risk" : "low"},
-  {"name" : "Aditya", "city" :"jangalore", "risk" : "low"},
-  {"name" : "Aditya", "city" :"langalore", "risk" : "low"},
-  {"name" : "Aditya", "city" :"kangalore", "risk" : "low"},
-  {"name" : "Aditya", "city" :"uangalore", "risk" : "high"},
+  {"name" : "Aditya", "city" :"bangalore", "risk" : "high"},
+  {"name" : "Aditya", "city" :"bangalore", "risk" : "high"},
+  {"name" : "Aditya", "city" :"bangalore", "risk" : "high"},
 ]
 
 @app.get("/customer")
 
 def testing_query(city : str, risk : str):
-  
+  filtered = [
+    c for c in customer_risk_level
+    if c["city"] == city and c["risk"] == risk
+  ]
+  return{
+    "No. of memebers found" : len(filtered),
+    "city" : city,
+    "id" : id,
+    "risk" : risk,
+    "results" : filtered
+  }
   
